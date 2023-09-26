26b9e39146b8c28dfadb9d32c35e491fcd2f541a
viktoriaukr <116932692+viktoriaukr@users.noreply.github.com>
2023-09-26 18:23:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ app = Flask(__name__)
 app.app_context().push()
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "postgresql://localhost/books_lover"
+    "DATABASE_URL", "postgres://qkzrbnpk:ZPcJumkR5UMihLSMTTNSRYFR2pVgzO4i@bubble.db.elephantsql.com/qkzrbnpk"
 )
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "it's a secret")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "2d24980166707adcbff5305e4175c393")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
